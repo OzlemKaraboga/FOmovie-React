d7dca37e2797b21bf5c2db13f89cf81a386f0e4c
OzlemKaraboga <ozlemkaraboga33@gmail.com>
2024-09-24 01:45:48 +0200

    Made changes to the styles

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
 import defaultImage from '../assets/default-card-image.png';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+
 export const Detail = () => {
     const apiUrl = import.meta.env.VITE_API_URL;
     const apiKey = import.meta.env.VITE_API_KEY;
     const { imdbID } = useParams();
+    const navigate = useNavigate();
 
     const url = `${apiUrl}?apikey=${apiKey}&i=${imdbID}`;
     const [movieInfo, setMovieInfo] = useState(null);
@@ -18,44 +20,48 @@ export const Detail = () => {
         getJson();
     }, []);
 
+    const handleCloseDetail = () => {
+        navigate('/');
+    };
+
     return (
         <>
             <Header />
-            {isLoading ? (
-                <p>Loading...</p>
-            ) : error ? (
-                <p>There was an error</p>
-            ) : (
-                <div style={{ backgroundColor: '#041322', color: '#FFFFFF' }}>
-                    <h1>{movieInfo && movieInfo.Title}</h1>
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <img
-                            src={
-                                movieInfo && movieInfo.Poster !== 'N/A'
-                                    ? movieInfo.Poster
-                                    : defaultImage
-                            }
-                            alt={movieInfo && movieInfo.Title}
-                        />
-                        <div>
-                            <p>Year: {movieInfo && movieInfo.Year}</p>
-                            <p>Language: {movieInfo && movieInfo.Language}</p>
-                            <p>Duration: {movieInfo && movieInfo.Runtime}</p>
-                            <p>Imdb Rating: {movieInfo && movieInfo.imdbRating}</p>
-                            <p>Director: {movieInfo && movieInfo.Director}</p>
-                            <p>Writer: {movieInfo && movieInfo.Writer}</p>
-                            <p>Actors: {movieInfo && movieInfo.Actors}</p>
-                            <p>Description: {movieInfo && movieInfo.Plot}</p>
+            <div className="movie-info-container">
+                <button onClick={handleCloseDetail} className="close-button">X</button>
+
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : error ? (
+                    <p>There was an error</p>
+                ) : (
+                    <div className="movie-detail">
+                        <h1>{movieInfo && movieInfo.Title}</h1>
+                        <div className='movie-container'>
+                            <img className='movie-info-img'
+                                src={
+                                    movieInfo && movieInfo.Poster !== 'N/A'
+                                        ? movieInfo.Poster
+                                        : defaultImage
+                                }
+                                alt={movieInfo && movieInfo.Title}
+
+                            />
+                            <div className="movie-info-content">
+                                <p>Year: {movieInfo && movieInfo.Year}</p>
+                                <p>Language: {movieInfo && movieInfo.Language}</p>
+                                <p>Duration: {movieInfo && movieInfo.Runtime}</p>
+                                <p>Imdb Rating: {movieInfo && movieInfo.imdbRating}</p>
+                                <p>Director: {movieInfo && movieInfo.Director}</p>
+                                <p>Writer: {movieInfo && movieInfo.Writer}</p>
+                                <p>Actors: {movieInfo && movieInfo.Actors}</p>
+                                <p>Description: {movieInfo && movieInfo.Plot}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
+
+                )}
+            </div>
             <Footer />
         </>
     );
